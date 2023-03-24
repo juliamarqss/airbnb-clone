00963cf61f55ed9ac52de7d31c0a38bfce32bbc7
Júlia Marques <juliamarques61@gmail.com>
2023-03-24 18:10:29 -0300

    feat: implementando a estrutura inicial da página

---
+++ src/pages/Customer/CheckoutCustomer/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CheckoutCustomer() {
+  return (
+    <div>
+      <h1>CheckoutCustomer</h1>
+    </div>
+  )
+}
---
+++ src/pages/Customer/DetailsPropertyCustomer/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DetailsPropertyCustomer() {
+  return (
+    <div>
+      <h1>DetailsPropertyCustomer</h1>
+    </div>
+  )
+}

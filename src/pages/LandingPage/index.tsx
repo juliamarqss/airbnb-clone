35e7ec1266724c8a0d129ef56a83218b2271f17f
Júlia Marques <juliamarques61@gmail.com>
2023-03-24 18:13:41 -0300

    feat: implementando a landing page

---
+++ src/pages/LandingPage/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Image from 'next/image'
+import Modal from '@/components/Modal'
+import Landing from '@/images/landing-page.jpg'
+import { useState } from 'react'
+
+export default function LandingPage() {
+  const [modal, setModal] = useState(false)
+
+  const handleClick = () => {
+    setModal(true)
+  }
+
+  return (
+    <div>
+      {
+        modal && <Modal />
+      }
+      <div>
+        <Image src={Landing} alt="Landing Page" />
+        <button onClick={handleClick}>Reserve aqui!</button>
+      </div>
+    </div>
+  )
+}

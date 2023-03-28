72fd8b818c3b9cedbe9382c7179092ca208ad303
Victor Martins <vhprestes@gmail.com>
2023-03-28 17:41:59 -0300

    feat: correções e pagina checkout

--- src/pages/Customer/DetailsPropertyCustomer/index.tsx
+++ src/pages/Customer/DetailsPropertyCustomer/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import data from '../../../mock/index.json'
 import Image from 'next/image'
+import Button from '@/components/Button'
+import { useRouter } from 'next/router'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+
 
 export default function DetailsPropertyCustomer() {
+  const router = useRouter()
+
   return (
     <div>
+      <Header />
       <h1>DetailsPropertyCustomer</h1>
       <div>
       <h2>{data.title}</h2>
@@ -19,6 +27,7 @@ export default function DetailsPropertyCustomer() {
       <Image src={data.img2} alt="Imagem 2"  width={300} height={300} unoptimized={true}/>
       <Image src={data.img3} alt="Imagem 3"  width={300} height={300} unoptimized={true}/>
       <p>Avaliação: {data.raiting}/50 ({data["raiting-average"]} estrelas)</p>
+      <p>Preço por diária: {data.price}</p>
       <h4>Comentários:</h4>
       <ul>
         {data.comments.map((comment, index) => (
@@ -35,7 +44,9 @@ export default function DetailsPropertyCustomer() {
       <p>Membro desde {data.owner.dateOfMember}</p>
       <p>Contato: {data.owner.contact}</p>
       <p>{data.owner.info}</p>
+      <Button title="Reserve ja!" onClick={() => router.push('/Customer/CheckoutCustomer')}  type='button' disabled={false}/>
     </div>
+    <Footer />
     </div>
   )
 }

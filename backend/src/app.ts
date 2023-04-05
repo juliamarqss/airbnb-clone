78c2b7a976b0dc5858660da6d342f529ad073af7
Victor Martins <vhprestes@gmail.com>
2023-04-05 14:45:48 -0300

    feat: estrutura API

---
+++ backend/src/app.ts
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+import express from 'express';
+import UserRouter from './routes/UserRoutes';
+import cors from 'cors';
+import app from '.';
+
+// mongoose.connect('mongodb://localhost:27017/meuBanco');
+
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/users', UserRouter);
+app.get('/', (_req, res) => res.send('Hello World!'));

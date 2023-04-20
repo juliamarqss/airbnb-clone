42542ac7706b3ffc127bd141a099c9290d71164a
juliamarqss <juliamarques61@gmail.com>
2023-04-20 16:42:34 -0300

    feat: adicionando props no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,14 @@ import Image from 'next/image'
 import Logo from '@/images/logo.png'
 import { useRouter } from 'next/router'
 
-export default function Header() {
+interface Props {
+  isVisible: boolean,
+  className?: string
+}
+
+export default function Header(props: Props) {
+  const { isVisible, className } = props;
+
   const [username, setUsername] = useState('');
   const router = useRouter()
 
@@ -15,9 +22,16 @@ export default function Header() {
 
   return (
     <div>
-      <Image src={Logo} alt="logo" height={30} />
-      <h2>{username}</h2>
-      <button onClick={() => router.push(`/Profile`)}>Profile</button>
-    </div>
+      <Image src={Logo} alt="logo" height={30} className={className}/>
+      {
+        isVisible && 
+        (
+        <div>
+          <h2>{username}</h2>
+          <button onClick={() => router.push(`/Profile`)}>Profile</button>
+        </div>
+        )
+      }
+   </div>
   )
 }

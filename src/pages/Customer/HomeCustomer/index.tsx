83ac32aa0189f4ca22e3931ba59424175b9177de
juliamarqss <juliamarques61@gmail.com>
2023-04-25 18:10:49 -0300

    feat: estilizando a pagina home

--- src/pages/Customer/HomeCustomer/index.tsx
+++ src/pages/Customer/HomeCustomer/index.tsx
@@ -2,15 +2,19 @@ import React from 'react'
 import Header from '@/components/Header'
 import data from '../../../mock/index.json'
 import CardProperty from '@/components/CardProperty'
+import Footer from '@/components/Footer'
 
 export default function HomeCustomer() {
 
   return (
     <div>
-      <Header isVisible={true} className="flex"/>
-      {data.map((item, index) => (
-        <CardProperty key={index} url={`http://localhost:3000/Customer/DetailsPropertyCustomer/${index}`} index={index} title={item.title} img1={item.img1} />
-      ))}
+      <Header isVisible={true} />
+      <div className="flex flex-wrap mx-10 my-5 justify-center">
+        {data.map((item, index) => (
+          <CardProperty key={index} url={`http://localhost:3000/Customer/DetailsPropertyCustomer/${index}`} index={index} title={item.title} img1={item.img1} />
+        ))}
+      </div>
+      <Footer/>
     </div>
   )
 }

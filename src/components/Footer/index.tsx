9b7f0ee30250bfa877aec1a34f2818e39642d944
juliamarqss <juliamarques61@gmail.com>
2023-04-25 18:10:18 -0300

    feat: estilizando o footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className="flex justify-center align-middle bottom-0 fixed bg-white w-full h-10">
+      <footer className="text-sm">
+        <p>
+          ©Desenvolvido por Giovanna Gomes, Júlia Marques e Victor Hugo
+        </p>
+      </footer>
     </div>
   )
 }

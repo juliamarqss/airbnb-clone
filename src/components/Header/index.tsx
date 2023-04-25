f300b93b73f2383af01dc5fb796473647808070a
juliamarqss <juliamarques61@gmail.com>
2023-04-25 18:09:02 -0300

    feat: estilizando o header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,35 +1,33 @@
 import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
 import Logo from '@/images/logo.png'
+import IconProfile from '@/images/icon-profile.png'
 import { useRouter } from 'next/router'
 
 interface Props {
   isVisible: boolean,
-  className?: string
 }
 
 export default function Header(props: Props) {
-  const { isVisible, className } = props;
-
-  const [username, setUsername] = useState('');
+  const { isVisible } = props;
   const router = useRouter()
 
-  useEffect(() => {
-    const usernameJSON = localStorage.getItem('username');
-    const username = usernameJSON ? JSON.parse(usernameJSON) : '';
-    setUsername(username);
-  }, []);
+  const handleLogout = () => {
+    router.push('/')
+  }
 
   return (
-    <div className="flex justify-between m-3 mx-5">
+    <div className="flex justify-between p-6 align-middle border border-b-[#c4c4c4ef]">
       <Image src={Logo} alt="logo" height={30} />
       {
         isVisible && 
         (
-        <div className="text-[#3f3f3fef] font-bold">
-          <h2>{username}</h2>
-          <button onClick={() => router.push(`/Profile`)}>Profile</button>
-        </div>
+        <nav className="flex align-middle text-[#3f3f3fef] font-bold">
+          <button type="button" onClick={() => router.push(`/Profile`)} className="mr-10">
+            <Image src={IconProfile} alt="Imagem do perfil" width={25} height={25}/>
+          </button>
+          <button type="button" onClick={handleLogout}>Sair</button>
+        </nav>
         )
       }
    </div>

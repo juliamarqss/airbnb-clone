a34015bfb92cb4c308c3de0433d1b74185a41141
juliamarqss <juliamarques61@gmail.com>
2023-04-25 18:11:28 -0300

    feat: estilizando CardProperty

--- src/components/CardProperty/index.tsx
+++ src/components/CardProperty/index.tsx
@@ -1,8 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import React from 'react'
-import data from '../../mock/index.json'
-import { useRouter } from 'next/router';
 
 interface Props {
   url: string,
@@ -17,11 +15,12 @@ export default function CardProperty(
   const { url, index, title, img1 } = props
 
   return (
-    <div>
-        <Link key={ index } href={ url } >
-          <Image src={ img1 } alt='foto' width={300} height={300} unoptimized={true} />
-          <h2>{ title }</h2>
-        </Link>
+    <div className="flex p-5 border border-black w-96">
+      <Link className="" key={ index } href={ url }>
+        <Image className="rounded-2xl" src={ img1 } alt="Propriedade" width={350} height={350} unoptimized={true} />
+        <h2 className="whitespace-pre-line font-semibold">{ title }</h2>
+        <p>ENDEREÇO</p>
+      </Link>
     </div>
   )
 }

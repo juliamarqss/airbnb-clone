005c85dbaece6aef1c6e7ffe1f9a074597cd029a
juliamarqss <juliamarques61@gmail.com>
2023-04-25 18:11:14 -0300

    feat: estilizando card da propriedade

--- src/pages/Customer/DetailsPropertyCustomer/[index].tsx
+++ src/pages/Customer/DetailsPropertyCustomer/[index].tsx
@@ -46,7 +46,7 @@ export default function DetailsPropertyCustomer() {
   const today = new Date();
   return (
     <div>
-      <Header isVisible={true}/>
+      <Header isVisible={true} />
       <CardDetailsProperty
       index={number}
       title={data[number]?.title}

dc054ecdb8dffe38398ac212ebafd06f158d8fd3
vhprestes <vhprestes@gmail.com>
2023-04-27 17:49:14 -0300

    feat: input filters

--- src/pages/Customer/HomeCustomer/index.tsx
+++ src/pages/Customer/HomeCustomer/index.tsx
@@ -1,16 +1,39 @@
-import React from 'react'
-import Header from '@/components/Header'
-import data from '../../../mock/index.json'
-import CardProperty from '@/components/CardProperty'
+import React, { useEffect, useState } from 'react';
+import Header from '@/components/Header';
+import data from '../../../mock/index.json';
+import CardProperty from '@/components/CardProperty';
 
 export default function HomeCustomer() {
+  const [filter, setFilter] = useState(''); // estado para armazenar as opções de filtro
+
+  const handleFilterChange = (value: any) => {
+    setFilter(value); // atualiza o estado de filtro
+  };
+
+  const filteredData = data.filter((item) => {
+    // verifica se o valor da propriedade categoria ou description contém a string de filtro
+    return item.categoria.toLowerCase().includes(filter.toLowerCase()) ||
+           item.description.toLowerCase().includes(filter.toLowerCase()) ||
+           item.details[0].toLowerCase().includes(filter.toLowerCase());
+  });
 
   return (
     <div>
       <Header />
-      {data.map((item, index) => (
-        <CardProperty key={index} url={`http://localhost:3002/Customer/DetailsPropertyCustomer/${index}`} index={index} title={item.title} img1={item.img1} />
+      <input
+        type="text"
+        placeholder="Pesquisar"
+        onChange={(e) => handleFilterChange(e.target.value)}
+      />
+      {filteredData.map((item, index) => (
+        <CardProperty
+          key={index}
+          url={`http://localhost:3002/Customer/DetailsPropertyCustomer/${index}`}
+          index={index}
+          title={item.title}
+          img1={item.img1}
+        />
       ))}
     </div>
-  )
+  );
 }
